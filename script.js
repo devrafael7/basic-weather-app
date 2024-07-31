cff16ea094792512b45d7d34309f5e047529e842
Rafael <rafarafa99944@gmail.com>
2024-07-31 04:10:12 -0300

    date added

--- script.js
+++ script.js
@@ -16,16 +16,13 @@ hTransitions.forEach(hTransition=>{
     observer.observe(hTransition);
 });
 
-
-const timeOfDayStatus = document.querySelector('.timeOfDayStatus'); 
-const dateInfo = document.querySelector('.dateInfo');
-
 function showOnScreen(data){
     const city = document.querySelector('.city');
     let weatherIcon = document.querySelector('.weatherIcon');
     let temp = document.querySelector('.temp');
     let description = document.querySelector('.description');
 
+    document.querySelector('.humidityText').innerHTML =`Umidade: ${data.main.humidity}%`
     city.innerHTML=`Tempo em ${data.name}`;
     temp.innerHTML= Math.floor(data.main.temp) + "°C";
     description.innerHTML = data.weather[0].description;
@@ -71,6 +68,50 @@ document.querySelector('.searchIcon').addEventListener('click', function searche
         wholeLoader.classList.add('hidden');
     }, 1000)
 })
+
+document.querySelectorAll('.noTranslate').forEach(noTranslates => {
+    noTranslates.setAttribute("translate", "no")
+});
+
+const timeOfDayStatus = document.querySelector('.timeOfDayStatus');
+const dateInfo = document.querySelector('.dateInfo');
+
+function updateDateTime() {
+    const now = new Date();
+    const datePart = now.toDateString(); 
+    const timePart = now.toTimeString().split(' ')[0]; 
+
+    const dateTime = `${datePart} ${timePart}`;
+    dateInfo.innerHTML = dateTime;
+
+    // Extrair a hora em formato numérico para comparação
+    const hours = now.getHours();
+
+    if (hours >= 18) {
+        timeOfDayStatus.innerHTML = `Boa Noite!`;
+    } else if (hours >= 12) {
+        timeOfDayStatus.innerHTML = `Boa Tarde!`;
+    } else if (hours >= 3) {
+        timeOfDayStatus.innerHTML = `Bom Dia!`;
+    } else {
+        timeOfDayStatus.innerHTML = `Boa Madrugada!`;
+    }
+}
+
+// Atualizar imediatamente
+updateDateTime();
+
+// Atualizar a cada segundo (1000 ms)
+setInterval(updateDateTime, 1000);
+
+
+
+
+
+
+
+
+
    
 
 
