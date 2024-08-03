20d1aff8a06d1f535889defdedd3b53ced543528
Rafael <rafarafa99944@gmail.com>
2024-08-02 22:49:13 -0300

    es6 error realy solved

--- script.js
+++ script.js
@@ -16,42 +16,74 @@
         observer.observe(hTransition);
     });
     
-function showOnScreen(data){
-    const city = document.querySelector('.city');
-    let weatherIcon = document.querySelector('.weatherIcon');
-    let temp = document.querySelector('.temp');
-    let description = document.querySelector('.description');
-
-    document.querySelector('.humidityText').innerHTML =`Umidade: ${data.main.humidity}%`
-    city.innerHTML=`Tempo em ${data.name}`;
-    temp.innerHTML= Math.floor(data.main.temp) + "°C";
-    description.innerHTML = data.weather[0].description;
+    function showOnScreen(data) {
+        const city = document.querySelector('.city');
+        let weatherIcon = document.querySelector('.weatherIcon');
+        let temp = document.querySelector('.temp');
+        let description = document.querySelector('.description');
     
-    if(data.weather[0].description === "algumas nuvens") {
-        weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/4814/4814293.png";
-    } else if (data.weather[0].description === "nublado"){
-        weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/1779/1779751.png";
-    } else if (data.weather[0].description === "nuvens dispersas"){
-        weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/1779/1779754.png";
-    } else if (data.weather[0].description === "céu limpo"){
-        weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/5904/5904053.png";
-    } else if (data.weather[0].description === "chuva leve"){
-        weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/4735/4735072.png";
-    } else {
-        weatherIcon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+        document.querySelector('.humidityText').innerHTML = `Umidade: ${data.main.humidity}%`;
+        city.innerHTML = `Tempo em ${data.name}`;
+        temp.innerHTML = Math.floor(data.main.temp) + "°C";
+        description.innerHTML = data.weather[0].description;
+    
+        const now = new Date();
+        const hours = now.getHours();
+    
+        if (data.weather[0].description === "algumas nuvens") {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/4814/4814293.png";
+        } else if (data.weather[0].description === "nublado") {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/1779/1779751.png";
+        } else if (data.weather[0].description === "nuvens dispersas") {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/1779/1779754.png";
+        } else if (data.weather[0].description === "céu limpo" && hours >= 18) {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/4735/4735038.png";
+        } else if (data.weather[0].description === "céu limpo") {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/5904/5904053.png";
+        } else if (data.weather[0].description === "chuva leve") {
+            weatherIcon.src = "https://cdn-icons-png.flaticon.com/128/4735/4735072.png";
+        } else {
+            weatherIcon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+        }
+        console.log(data);
+        console.log(hours);
     }
-    console.log(data)
-}
-
-let key = "807b807190283b8bbf44326c1227677a";
-
-async function searchCity(theCity){
-    let data = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + theCity + "&appid=" + key + "&lang=pt_br" + "&units=metric").then(
-        (answer => answer.json())
-    )
-
-    showOnScreen(data);
-};
+    
+    const timeOfDayStatus = document.querySelector('.timeOfDayStatus');
+    const dateInfo = document.querySelector('.dateInfo');
+    
+    function updateDateTime() {
+        const now = new Date();
+        const hours = now.getHours();
+        const datePart = now.toDateString();
+        const timePart = now.toTimeString().split(' ')[0];
+    
+        const dateTime = `${datePart} ${timePart}`;
+        dateInfo.innerHTML = dateTime;
+    
+        if (hours >= 18) {
+            timeOfDayStatus.innerHTML = `Boa Noite!`;
+        } else if (hours >= 12) {
+            timeOfDayStatus.innerHTML = `Boa Tarde!`;
+        } else if (hours >= 3) {
+            timeOfDayStatus.innerHTML = `Bom Dia!`;
+        } else {
+            timeOfDayStatus.innerHTML = `Boa Madrugada!`;
+        }
+    }
+    
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+    
+    let key = "807b807190283b8bbf44326c1227677a";
+    
+    async function searchCity(theCity) {
+        let data = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + theCity + "&appid=" + key + "&lang=pt_br" + "&units=metric").then(
+            (answer => answer.json())
+        )
+    
+        showOnScreen(data);
+    }
 
 const wholeLoader = document.querySelector('.wholeLoader');
 
@@ -73,33 +105,11 @@ document.querySelectorAll('.noTranslate').forEach(noTranslates => {
     noTranslates.setAttribute("translate", "no")
 });
 
-const timeOfDayStatus = document.querySelector('.timeOfDayStatus');
-const dateInfo = document.querySelector('.dateInfo');
-
-function updateDateTime() {
-    const now = new Date();
-    const datePart = now.toDateString(); 
-    const timePart = now.toTimeString().split(' ')[0]; 
-
-    const dateTime = `${datePart} ${timePart}`;
-    dateInfo.innerHTML = dateTime;
-
-    const hours = now.getHours();
-
-    if (hours >= 18) {
-        timeOfDayStatus.innerHTML = `Boa Noite!`;
-    } else if (hours >= 12) {
-        timeOfDayStatus.innerHTML = `Boa Tarde!`;
-    } else if (hours >= 3) {
-        timeOfDayStatus.innerHTML = `Bom Dia!`;
-    } else {
-        timeOfDayStatus.innerHTML = `Boa Madrugada!`;
-    }
-}
 
 
-updateDateTime();
-setInterval(updateDateTime, 1000);
+
+
+
 
 
 
